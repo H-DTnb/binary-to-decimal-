7eaaa92c52f41d4b6dde05d47bebbbc3219353c5
Hamid <tkhamid25@gmail.com>
2024-11-01 12:42:40 -0700

    Add files via upload

---
+++ binary to decimal/binary to decimal/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binary_to_decimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dNumb; string binary; 
+              Console.Write("enter binary number :");
+                 binary = Console.ReadLine();
+                     dNumb = Convert.ToInt32(binary , 2);
+                           Console.WriteLine("decimal of your binary number is : " + dNumb);
+                                Console.ReadKey();                   
+        }
+    }
+}
